c0108f841fc1c4fc95add2abd405b6b3e0baa3df
girkar27 <jaigirkar31@gmail.com>
2020-05-27 14:19:20 +0530

    Redux initializer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main  from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
+
 
 class App extends Component {
  
   render() {
     return (
+    	<Provider store={store}>
     	<BrowserRouter>
 	      <div className="App">
 	        <Main/>
 	      </div>
     	</BrowserRouter>
+    	</Provider>
     );
   }
 }
